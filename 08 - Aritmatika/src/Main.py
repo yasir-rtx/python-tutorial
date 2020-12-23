413a99b0518ce0e9f226e99e977a4e9c8135abca
yasir-rtx <yelfvankarlenstain@gmail.com>
2020-12-23 20:22:34 +0700

    EPISODE 8 DONE!!!!!

---
+++ 08 - Aritmatika/src/Main.py
@@ -0,0 +1,33 @@
+import os
+os.system('cls')
+
+# Operasi Aritmatika
+x = 10
+y = 4
+
+# Penjumlahan
+print(x, '+', y, '=', x + y)
+
+# Pengurangan
+print(x, '-', y, '=', x - y)
+
+# Perkalian
+print(x, '*', y, '=', x * y)
+
+# Pembagian
+print(x, '/', y, '=', x / y) #otomatis jadi float
+
+# Eksponen
+print(x, '**', y, '=', x ** y)
+
+# Modulus
+print(x, '%', y, '=', x % y)
+
+# Floor Division
+print(x, '//', y, '=', x // y) #hasil akan dibulatkan ke bawah
+
+# Prioritas Operasi Aritmatika
+# 1. () --> Paling OP, wkwkwk
+# 2. **
+# 3. *, /, //
+# 4. +, -
